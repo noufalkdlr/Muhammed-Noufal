2ff487feccaebe2dcb7a892ecf675c6f400477a3
Muhammed Noufal <noufalkakdlr@gmail.com>
2021-08-18 08:56:41 +0530

    change

--- js/navbar.js
+++ js/navbar.js
@@ -4,3 +4,13 @@ $(document).ready(function(){
       $('ul li').toggleClass("hide");
     });
   });
+
+
+    // Look for .hamburger
+    var hamburger = document.querySelector(".hamburger");
+    // On click
+    hamburger.addEventListener("click", function() {
+      // Toggle class "is-active"
+      hamburger.classList.toggle("is-active");
+      // Do something else, like open/close menu
+    });
